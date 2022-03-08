3391716847a8e125deca08c2c19fb6c411f35ecb
Carly Hutson <chutson1@uwyo.edu>
2022-03-07 17:15:57 -0700

    started work on form

--- web-app/src/components/settings/Settings.js
+++ web-app/src/components/settings/Settings.js
@@ -1,8 +1,12 @@
 import "./settings.css";
+import SettingsForm from "../settings-form/settings-form.js"
 
 function Settings() {
     return (
-        <div className="settings">Settings Form?</div>
+        <div className="settings">
+            <h3>SETTINGS</h3>
+            <SettingsForm />
+        </div>
     )
 }
 export default Settings
